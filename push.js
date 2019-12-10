7936ee59ff55d2058c407164df94b9ddba48be35
Yaroslav Yatsiuk <yyatsiuk@cisco.com>
2019-12-10 15:15:52 +0200

    test notification_2

--- push.js
+++ push.js
@@ -13,7 +13,12 @@ push.setVapidDetails(
 );
 
 let sub = {
-
-};
+"endpoint":"https://fcm.googleapis.com/fcm/send/fc0R4L7PBLI:APA91bHLnDd6e-dIKnQrDubNPm8e5O8OHQpMj6zRGGUw_m2QgQrlTwvK8Y4wrWY6Fq8Gyg_1wopejLhHSuUwK0htAA9x-oYchpKbOkM6CmkbzLR9kkc6n3-HoSedLxcLF5Thi_gjmeW1",
+"expirationTime":null,
+"keys":{
+  "p256dh":"BGjmrzLPYx_iqQHVUMBDrOEtt7UVnKCMKQfvNFvDpHAgVFiKqfL4HviZDyk9QipDy7bfjUKaUl_dehWeQSHyPdU",
+  "auth":"Dz-SWvQAxSmjh3lAGms3xg"
+}
+}
 
 push.sendNotification(sub, 'test message');
